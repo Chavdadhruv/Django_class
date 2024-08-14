f36c0804ab532149b050af22b0620ffe608501fb
Chavda_Dhruv447 <149055293+Chavdadhruv@users.noreply.github.com>
2024-08-14 10:40:30 +0530

    Add files via upload

---
+++ Python_T4/moviereviewsproject/movie/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Movie
+# Create your views here.
+
+def home(request):
+    searchTerm=request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'name':'KMS','searchTerm':searchTerm, 'movie':movies})
+
+def about(request):
+    return HttpResponse('<h1> Welcome to About Page </h1>')
+
+def signup(request):
+    email=request.GET.get('email')
+    return render(request, 'signup.html', {'email':email})
+
---
+++ Python_T4/moviereviewsproject/news/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import News
+# Create your views here.
+def news(request):
+    news = News.objects.all().order_by('-date')
+    return render(request,'news.html',{'news':news})
